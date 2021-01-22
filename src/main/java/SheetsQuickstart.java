65977d3c2f3f8ac6da4d21799ab871e63e6d3e5b
it-school <GITHUB it@osvita.od.ua>
2021-01-22 19:50:52 +0200

    Syntax enhancements

--- src/main/java/SheetsQuickstart.java
+++ src/main/java/SheetsQuickstart.java
@@ -23,70 +23,49 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
-public class SheetsQuickstart
-{
-	private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-	private static final String TOKENS_DIRECTORY_PATH = "tokens";
+public class SheetsQuickstart {
+    private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-	/**
-	 * Global instance of the scopes required by this quickstart.
-	 * If modifying these scopes, delete your previously saved tokens/ folder.
-	 */
-	private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
-	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        final String spreadsheetId = "1aYKBsScBRVLUFTNR_iB724qNEcT_8cuuAraRsAq_Ge8";
+        final String range = "Sheet1!A1:D";
+        List<List<Object>> dataList = readFromSheet(HTTP_TRANSPORT, spreadsheetId, range);
 
-	/**
-	 * Prints the names and majors of students in a sample spreadsheet:
-	 * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-	 */
-	public static void main(String... args) throws IOException, GeneralSecurityException
-	{
-		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		final String spreadsheetId = "1aYKBsScBRVLUFTNR_iB724qNEcT_8cuuAraRsAq_Ge8";
-		final String range = "Sheet1!A1:D";
-		List<List<Object>> dataList = readFromSheet(HTTP_TRANSPORT, spreadsheetId, range);
+        updateValues(HTTP_TRANSPORT, spreadsheetId, "Sheet1!A" + (dataList.size() + 1), dataList);
+    }
 
-		updateValues(HTTP_TRANSPORT, spreadsheetId, "Sheet1!A"+(dataList.size()+1), dataList);
+    private static List<List<Object>> readFromSheet(NetHttpTransport HTTP_TRANSPORT, String spreadsheetId, String range) throws GeneralSecurityException, IOException {
+        // Build a new authorized API client service.
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT)).setApplicationName(APPLICATION_NAME).build();
+        ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            for (List row : values) {
+                // Print columns A and E, which correspond to indices 0 and 4.
+                try {
+                    System.out.printf("%s, %s, %s\n", row.get(0), row.get(1), row.get(2), row.get(3));
+                } catch (Exception e) {
 
-	}
+                }
+            }
+        }
+        return values;
+    }
 
-	private static List<List<Object>> readFromSheet(NetHttpTransport HTTP_TRANSPORT, String spreadsheetId, String range) throws GeneralSecurityException, IOException
-	{
-		// Build a new authorized API client service.
-		Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME)
-				.build();
-		ValueRange response = service.spreadsheets().values()
-				.get(spreadsheetId, range)
-				.execute();
-		List<List<Object>> values = response.getValues();
-		if (values == null || values.isEmpty()) {
-			System.out.println("No data found.");
-		}
-		else {
-			for (List row : values) {
-				// Print columns A and E, which correspond to indices 0 and 4.
-				try {
-					System.out.printf("%s, %s, %s\n", row.get(0), row.get(1), row.get(2), row.get(3));
-				}
-				catch (Exception e)
-				{
+    public static UpdateValuesResponse updateValues(NetHttpTransport HTTP_TRANSPORT, String spreadsheetId, String range,
+                                                    List<List<Object>> _values)
+            throws IOException {
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME).build();
 
-				}
-			}
-		}
-		return values;
-	}
-
-	public static UpdateValuesResponse updateValues(NetHttpTransport HTTP_TRANSPORT, String spreadsheetId, String range,
-	                                                List<List<Object>> _values)
-			throws IOException
-	{
-		Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME).build();
-
-		// [START sheets_update_values]
+        // [START sheets_update_values]
 /*
 		List<List<Object>> values = Arrays.asList(
 				Arrays.asList(
@@ -99,44 +78,37 @@ public class SheetsQuickstart
 		values = _values;
 		// [END_EXCLUDE]
 */
-		ArrayList allValues = new ArrayList<String>();
-		allValues.add(Calendar.getInstance().getTime().toString());
-		_values.add(allValues);
-		ValueRange body = new ValueRange().setValues(_values);
+        ArrayList allValues = new ArrayList<String>();
+        allValues.add(Calendar.getInstance().getTime().toString());
+        _values.add(allValues);
+        ValueRange body = new ValueRange().setValues(_values);
 
-		String valueInputOption = "RAW";
-		UpdateValuesResponse result = service.spreadsheets().values().update(spreadsheetId, range, body)
-				.setValueInputOption(valueInputOption).execute();
-		System.out.printf("%d cells updated.", result.getUpdatedCells());
-		// [END sheets_update_values]
-		return result;
-	}
+        String valueInputOption = "RAW";
+        UpdateValuesResponse result = service.spreadsheets().values().update(spreadsheetId, range, body).setValueInputOption(valueInputOption).execute();
+        System.out.printf("%d cells updated.", result.getUpdatedCells());
+        // [END sheets_update_values]
+        return result;
+    }
 
-	/**
-	 * Creates an authorized Credential object.
-	 *
-	 * @param HTTP_TRANSPORT The network HTTP Transport.
-	 *
-	 * @return An authorized Credential object.
-	 *
-	 * @throws IOException If the credentials.json file cannot be found.
-	 */
-	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException
-	{
-		// Load client secrets.
-		InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-		if (in == null) {
-			throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-		}
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-				HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-				.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-				.setAccessType("offline")
-				.build();
-		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-	}
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH))).setAccessType("offline").build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
 }
